346c87e77a535aa68adb9ef1294e5fe2f3463d84
Fatur Muhammad <fatur_muhammad@bk.ru>
2023-09-20 23:33:48 +0700

    fixing body

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
-    import {Router, Link, Route} from 'svelte-routing';
-    import Login from "./routes/Login.svelte";
-    import Home from "./routes/Home.svelte";
-    export let url = "";
+	import {Router, Link, Route} from 'svelte-routing';
+	import Login from "./routes/Login.svelte";
+	import Home from "./routes/Home.svelte";
+	export let url = "";
 </script>
 
-<body>
-    <Router {url}>
-        <div>
-          <Route path="/dashboard" component={Home} />
-          <Route path="/"><Login /></Route>
-        </div>
-      </Router>
-</body>
+<Router {url}>
+	<div>
+		<Route path="/dashboard" component={Home} />
+		<Route path="/"><Login /></Route>
+	</div>
+</Router>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,25 @@ module.exports = {
     "./node_modules/tw-elements/dist/js/**/*.js"
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      'xxl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+
+      'xxxl': '1920px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {},
   },
   darkMode: "class",
