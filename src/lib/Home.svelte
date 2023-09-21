f50b366d409adc87587d1557ea746a56de103cbc
Fatur Muhammad <fatur_muhammad@bk.ru>
2023-09-22 00:10:38 +0700

    add search button

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+    import type { User } from './interface/user.interface';
     import { onMount } from "svelte";
     import Navigation from "./Navigation.svelte";
 
-    export let users = [] as any | null
+    let search = ''
+    let users = [] as User[]
 
     onMount(() => {
         fetch('http://localhost:3000/user/', 
@@ -18,14 +20,40 @@
             if (res.status === 200)
             {
                 users = await res.json()
-                console.log(users)
             }
+            else
+            {
+                window.location.href = "/"
+            }
+        }).
+        catch((e) => 
+        {
+            window.location.href = "/"
         })
     });
+
+    const onSubmit = () =>{
+        users = users.filter(param => param.username === search)
+    }
 </script>
 
 <Navigation>
     <div class="overflow-x-auto">
+        <form on:submit|preventDefault={onSubmit}>
+            <div class="pt-2 relative mx-auto text-gray-600">
+                <input class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+                    type="search" name="search" placeholder="Search" bind:value={search}>
+                <button type="submit" class="absolute right-0 top-0 mt-5 mr-4">
+                    <svg class="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px"
+                        viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve"
+                        width="512px" height="512px">
+                        <path
+                        d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+                    </svg>
+                </button>
+            </div>
+        </form>
         <table class="table">
           <!-- head -->
             <thead>
---
+++ src/lib/interface/user.interface.ts
@@ -0,0 +1,28 @@
+interface Access {
+    create: boolean;
+    read: boolean;
+    update: boolean;
+    delete: boolean;
+    suspend: boolean;
+    system: boolean;
+}
+
+interface Role {
+    _id: string;
+    level: number;
+    name: string;
+    access: Access;
+}
+
+export interface User {
+    _id: string
+    fullname: string;
+    username: string;
+    email: string;
+    computer_name: string;
+    hardware_id: string;
+    image: string;
+    remember_token: string;
+    expired: string;
+    role: Role;
+}
