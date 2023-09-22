01163ab7949a4675ca0489d6f6fd366163f3afeb
Fatur Muhammad <fatur_muhammad@bk.ru>
2023-09-22 21:49:58 +0700

    add card

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -50,41 +50,36 @@
 </script>
 
 <Navigation>
-    <div class="container">
-        <div class="grid grid-cols-6 gap-1">
-            <div class="col-span-4 col-start-4 mr-12 border-2 border-slate-600">
-                <div class="stats shadow">
-      
-                    <div class="stat">
-                        <div class="stat-figure text-secondary">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                        </div>
-                        <div class="stat-title">Downloads</div>
-                        <div class="stat-value">31K</div>
-                        <div class="stat-desc">Jan 1st - Feb 1st</div>
-                    </div>
-                    
-                    <div class="stat">
-                        <div class="stat-figure text-secondary">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
-                        </div>
-                        <div class="stat-title">New Users</div>
-                        <div class="stat-value">4,200</div>
-                        <div class="stat-desc">↗︎ 400 (22%)</div>
-                    </div>
-                    
-                    <div class="stat">
-                        <div class="stat-figure text-secondary">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
-                        </div>
-                        <div class="stat-title">New Registers</div>
-                        <div class="stat-value">1,200</div>
-                        <div class="stat-desc">↘︎ 90 (14%)</div>
+    <div class="2xl:container mx-auto my-auto">
+        <div class="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+
+            <div class="rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
+                <div class="card w-96 mx-auto bg-base-100 shadow-sm border-2 border-slate-600">
+                    <div class="card-body">
+                        <h2 class="card-title">Card title!</h2>
+                        <p>a</p>
                     </div>
                 </div>
             </div>
-            <div class="col-start-2 col-end-3 h-56 border-2 border-slate-600">
-                <div class="chat chat-start">
+            <div class="col-start-3">
+                <div class="card w-96 left-0 bg-base-100 shadow-sm border-2 border-slate-600">
+                    <div class="card-body">
+                        <h2 class="card-title">Card title!</h2>
+                        <p>b</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-start-4">
+                <div class="card w-96 mx-auto bg-base-100 shadow-sm border-2 border-slate-600">
+                    <div class="card-body">
+                        <h2 class="card-title">Card title!</h2>
+                        <p>c</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-start-1 col-end-4 h-56 mt-10 border-2 border-slate-600">
+                <div class="xl:chat chat-start">
                     <div class="chat-image avatar">
                         <div class="w-10 rounded-full">
                             <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="" />
@@ -96,7 +91,7 @@
                     </div>
                     <div class="chat-bubble">You were the Chosen One!</div>
                         <div class="chat-footer opacity-50">
-                        Delivered
+                            Delivered
                         </div>
                     </div>
                     <div class="chat chat-end">
@@ -112,8 +107,8 @@
                         <div class="chat-bubble">I hate you!</div>
                         <div class="chat-footer opacity-50">
                             Seen at 12:46
-                        </div>
-                  </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -33,7 +33,7 @@
         <!-- Page content here -->
         <div class="flex flex-col h-screen justify-between">
             <header class="mb-10 h-10">
-                <div class="navbar bg-base-100">
+                <nav class="navbar bg-base-100">
                     <div class="flex-none">
                         <label for="my-drawer" class="btn btn-square btn-ghost">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
@@ -70,7 +70,7 @@
                             </div>
                         </ul>
                     </div>
-                </div>
+                </nav>
             </header>
             <main class="mb-auto h-10">
                 <div>
@@ -91,7 +91,7 @@
                     </dialog>
                 </div>
             </main>
-            <footer class="footer footer-center p-4 h-10 bg-base-300 text-base-content">
+            <footer class="footer footer-center fixed inset-x-0 bottom-0">
                 <aside>
                   <p>Copyright © 2023 - All right reserved by Ellohim Ltd</p>
                 </aside>
